05d4ee40eb06fe532e9d83323b1fac01e630bb18
ChenMyae KayKhaing <112561446+Chen-Myae2K@users.noreply.github.com>
2025-03-01 11:46:35 +0800

    err: component file typo

--- src/features/home/pages/HomePage.jsx
+++ src/features/home/pages/HomePage.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import HeroSection from "../components/herosection";
 import HomeSection1 from "../components/HomeSection1";
+import HeroSection from "../components/HeroSection"
 import TextReveal from "../../../components/TextReveal";
 import HomeSection2 from "../components/HomeSection2";
+import HomeSection3 from "../components/HomeSection3";
 
 const HomePage = () => {
   return (
     <>
-      <HeroSection />
+      <HeroSection/>
       <HomeSection1 />
       <HomeSection2/>
+      <HomeSection3/>
     </>
   );
 };
