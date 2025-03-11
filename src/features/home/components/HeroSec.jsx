96042a3b3f039d105434dfaf6d245b8ce1350aa7
ChenMyae KayKhaing <112561446+Chen-Myae2K@users.noreply.github.com>
2025-03-12 00:04:47 +0800

    req: img change

--- src/features/home/components/HeroSec.jsx
+++ src/features/home/components/HeroSec.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import cargoship from "../../../images/cargoship.png";
 import cargoshipmobile from "../../../images/cargoshipvertical.png";
-import khhlogo from "../../../images/khhlogo.png";
 import Container from "../../../components/Container";
 
 const HeroSec = () => {
   return (
     <section className="font-instrument ">
-      {/* <div className="lg:hidden block mb-10">
-        <img src={khhlogo} className="w-[20vw]" alt="" />
-      </div> */}
+      <div className="lg:block hidden">
       <Container>
         {" "}
         <p className="text-4xl  mt-24 mb-12 lg:text-6xl text-black font-medium tracking-tighter leading-normal lg:leading-tight">
@@ -19,11 +16,12 @@ const HeroSec = () => {
           <span className="text-clean lg:block"> safeguard your products</span>
         </p>
       </Container>
+      </div>
       <div className="lg:block hidden">
         <img src={cargoship} className="" alt="" />
       </div>
       <div className="lg:hidden block">
-        <img src={cargoshipmobile} className="w-full" alt="" />
+        <img src={cargoshipmobile} className="w-full p-5" alt="" />
       </div>
     </section>
   );
