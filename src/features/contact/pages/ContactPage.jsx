9973e4a098ab520d7a2f54885319dffcd57f4c15
ChenMyae KayKhaing <112561446+Chen-Myae2K@users.noreply.github.com>
2025-03-11 16:43:12 +0800

    contact err done

--- src/features/contact/pages/ContactPage.jsx
+++ src/features/contact/pages/ContactPage.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ContactSection from "../components/ContactSection";
+import Lenis from "lenis";
 
 const ContactPage = () => {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    // Scroll to the top after Lenis initializes
+    setTimeout(() => {
+      lenis.scrollTo(0, { immediate: true });
+    }, 100);
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <>
       <ContactSection />
